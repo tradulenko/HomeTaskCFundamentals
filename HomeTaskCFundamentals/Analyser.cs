d8c48771bb4b27b5765456fc1f9b83cc69f72a14
Taras Radulenko <Taras_Radulenko@epam.com>
2023-05-05 22:46:35 +0300

    Add project files.

---
+++ HomeTaskCFundamentals/Analyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskCFundamentals
+{
+    public class Analyser
+    {
+        public static bool AnalyseNumber(int inputNumber, int thinkedNumber)
+        {
+            bool isNumberedFound = false;
+            if (inputNumber < thinkedNumber)
+            {
+                Console.WriteLine("My number is bigger..");
+            }
+            else if (inputNumber > thinkedNumber)
+            {
+                Console.WriteLine("My number is smaller..");
+            }
+            else
+            {
+                Console.WriteLine("Bullseye!");
+                isNumberedFound = true;
+            }
+            return isNumberedFound;
+        }
+    }
+}
---
+++ HomeTaskCFundamentals/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using HomeTaskCFundamentals;
+
+var rnd = new Random();
+bool wantToExit;
+do
+{   
+    wantToExit = false;
+    int thinkedNumber = rnd.Next(0, 50);
+    Console.WriteLine(thinkedNumber);
+
+    bool isNumberedFound = false;
+
+    do
+    {
+        Console.WriteLine("\nEnter your number (for exit enter 'P'):");
+        int inputNumber;
+        try
+        {
+            string? inputedValue = Console.ReadLine();
+            if ((inputedValue != null) && inputedValue.Equals("P", StringComparison.OrdinalIgnoreCase))
+            {
+                wantToExit = true;
+                break;
+            }
+
+            inputNumber = Convert.ToInt32(inputedValue);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("You should enter a number! Try again");
+            continue;
+        }
+
+        isNumberedFound = Analyser.AnalyseNumber(inputNumber, thinkedNumber);
+
+    } while (!isNumberedFound);
+
+    if (!wantToExit)
+    {
+        Console.WriteLine("\nDo you want exit Press Y");
+        wantToExit = (Console.ReadLine() ?? "").Equals("y", StringComparison.OrdinalIgnoreCase);
+    }
+} while (!wantToExit);
+
+Console.WriteLine("GoodBuy!");
+
