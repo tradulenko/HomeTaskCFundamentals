e80d5744d6aa691d1f774cb1d7d605d5c68a61bc
Taras Radulenko <Taras_Radulenko@epam.com>
2023-05-11 13:47:17 +0300

    HomeWork 2 - Generics

---
+++ HomeTaskGenerics/Interface1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskGenerics
+{
+    internal interface INumber<T>
+    {
+        T Add(T Number);
+        T Sub(T Number);
+        T Mul(int Constant);
+        
+    }
+
+    class Integer : INumber<Integer>
+    {
+        int Value;
+        public Integer(int Value)
+        {
+            this.Value = Value;
+        }
+
+        public Integer Add(Integer Number)
+        {
+            return new Integer(this.Value + Number.Value);
+        }
+
+        public Integer Mul(int Constant)
+        {
+            return new Integer(this.Value * Constant);
+        }
+
+        public Integer Sub(Integer Number)
+        {
+            return new Integer(this.Value - Number.Value);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    class Double : INumber<Double>
+    {
+        public double Value;
+        public Double(double Value)
+        {
+            this.Value = Value;     
+        }
+
+        public Double Add(Double Number)
+        {
+            return new Double(this.Value + Number.Value);   
+        }
+
+        public Double Mul(int Constant)
+        {
+            return new Double(this.Value * Constant);
+        }
+
+        public Double Sub(Double Number)
+        {
+            return new Double(this.Value - Number.Value);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    class Decimal : INumber<Decimal> { 
+        public decimal Value;
+
+        public Decimal(decimal Value)
+        {
+            this.Value = Value;
+        }
+
+        public Decimal Add(Decimal Number)
+        {
+            return new Decimal(this.Value + Number.Value);  
+        }
+
+        public Decimal Mul(int Constant)
+        {
+            return new Decimal(this.Value * Constant);
+        }
+
+        public Decimal Sub(Decimal Number)
+        {
+            return new Decimal(this.Value - Number.Value);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+        
+
+    }
+
+    class Matrix<T> where T : INumber<T> 
+    {
+        T[,] internalMatrix;
+
+        public Matrix(T[,] matrix)
+        {
+            this.internalMatrix = matrix;
+        }
+
+        public bool IsSizeTheSame(T[,] matrix)
+        {
+            if ((internalMatrix.GetLength(0) == matrix.GetLength(0)) && (internalMatrix.GetLength(1) == matrix.GetLength(1))) 
+            {
+                return true; 
+            }
+            return false;
+        }
+
+       
+        public static void Print(Matrix<T>? matrixForPrint)
+        {
+            if (matrixForPrint is not null)
+            {
+                Console.WriteLine("\nMatrix : ");
+                for (int i = 0; i < matrixForPrint.internalMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrixForPrint.internalMatrix.GetLength(1); j++)
+                    {
+                        System.Console.Write(matrixForPrint.internalMatrix[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            } else
+            {
+                Console.WriteLine("Can`t print null matrix!");
+            }
+        }
+
+        public Matrix<T>? AddMatix(Matrix<T> secondMatrix)
+        {
+            if (secondMatrix is not null && IsSizeTheSame(secondMatrix.internalMatrix)) 
+            {
+                T[,] tempMatrix = new T[internalMatrix.GetLength(0), internalMatrix.GetLength(1)];
+                for (int i = 0; i < internalMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < internalMatrix.GetLength(1); j++)
+                    {
+                        tempMatrix[i, j] = internalMatrix[i, j].Add(secondMatrix.internalMatrix[i, j]);
+                    }
+                }
+
+
+                return new Matrix<T>(tempMatrix);
+            } else
+            {
+                Console.WriteLine("Matrixs have different size");
+                return null;
+            }
+        }
+
+        public Matrix<T>? SubstructMatix(Matrix<T> secondMatrix)
+        {
+            if (secondMatrix is not null && IsSizeTheSame(secondMatrix.internalMatrix))
+            {
+                T[,] tempMatrix = new T[internalMatrix.GetLength(0), internalMatrix.GetLength(1)];
+                for (int i = 0; i < internalMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < internalMatrix.GetLength(1); j++)
+                    {
+                        tempMatrix[i, j] = internalMatrix[i, j].Sub(secondMatrix.internalMatrix[i, j]);
+                    }
+                }
+
+
+                return new Matrix<T>(tempMatrix);
+            }
+            else
+            {
+                Console.WriteLine("Matrixs have different size");
+                return null;
+            }
+        }
+
+        public Matrix<T>? MultByConstant(int constant)
+        {
+            T[,] tempMatrix = new T[internalMatrix.GetLength(0), internalMatrix.GetLength(1)];
+            for (int i = 0; i < internalMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < internalMatrix.GetLength(1); j++)
+                {
+                    tempMatrix[i, j] = internalMatrix[i, j].Mul(constant);
+                }
+            }
+
+
+            return new Matrix<T>(tempMatrix);
+        }
+    }
+
+}
---
+++ HomeTaskGenerics/Program.cs
@@ -0,0 +1,57 @@
+﻿using HomeTaskGenerics;
+using Double = HomeTaskGenerics.Double;
+using Decimal = HomeTaskGenerics.Decimal;
+
+Integer newInteger = new Integer(15);
+Integer newInteger1 = new Integer(6);
+Double newDouble = new Double(2.5);
+Decimal newDecimal = new Decimal(23);
+
+Console.WriteLine($"New Integer: {newInteger}");
+Console.WriteLine(newInteger.Sub(newInteger1));
+Console.WriteLine(newDouble);
+Console.WriteLine(newDecimal);
+
+Integer[,] matrix1 = new Integer[2, 2]
+{
+    { new Integer(2), newInteger1 },
+    { new Integer(2), newInteger1 }
+};
+
+Integer[,] matrix2 = new Integer[3, 2]
+{
+    { new Integer(1), new Integer(1) },
+    { new Integer(1), new Integer(1) },
+    { new Integer(1), new Integer(1) }
+};
+
+Integer[,] matrix3 = new Integer[2, 2]
+{
+    { new Integer(1), new Integer(1) },
+    { new Integer(1), new Integer(1) }
+};
+
+
+Matrix<Integer> matrixInteger = new Matrix<Integer>(matrix1);
+Matrix<Integer>.Print(matrixInteger);
+
+// Error when try to work with matrix with different size
+matrixInteger.AddMatix(new Matrix<Integer>(matrix2));
+
+
+// Try method from Matrix
+Matrix<Integer>.Print(matrixInteger.AddMatix(new Matrix<Integer>(matrix1)));
+
+Matrix<Integer>.Print(matrixInteger.SubstructMatix(new Matrix<Integer>(matrix3)));
+
+Matrix<Integer>.Print(matrixInteger.MultByConstant(2));
+
+//Try Matrix Double
+Double[,] matrixDouble = new Double[2, 2]
+{
+    { new Double(1.5), new Double(1) },
+    { new Double(1.5), new Double(1) }
+};
+
+
+Matrix<Double>.Print(new Matrix<Double>(matrixDouble).MultByConstant(3));
