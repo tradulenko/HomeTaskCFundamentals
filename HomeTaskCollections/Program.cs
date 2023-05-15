f536501e2cb2cabfab06fb57d0bfa56eab758931
Taras Radulenko <Taras_Radulenko@epam.com>
2023-05-15 16:51:25 +0300

    Home task 1: Collections

---
+++ HomeTaskCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using HomeTaskCollections;
+using System.Collections;
+
+namespace HTCollections
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            //Home task 1: Collections
+            List<ShoppingItem> shoppingItems = new();
+            shoppingItems.Add(new ShoppingItem() { price = 16.5, quantity = 5, name = "Tomato" });
+            shoppingItems.Add(new ShoppingItem() { price = 10.5, quantity = 15, name = "Potato" });
+
+            foreach (ShoppingItem item in shoppingItems)
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ HomeTaskCollections/ShoppingItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskCollections
+{
+    public class ShoppingItem
+    {
+        public string? name { get; init; }
+        public double price { get; init;  }
+        public int quantity { get; init; }
+
+        public override string ToString()
+        {
+            return string.Format("ShoppingItem: name -> '{0}' price -> {1} quantity {2}", name, price, quantity);
+        }
+    }
+}
